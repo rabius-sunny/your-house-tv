9c54a6a7abfd40e8f2a3aaaaa8cd0565b039561a
rabius-sunny <rabiussunnyc3x@gmail.com>
2025-06-17 16:01:41 +0600

    add new video

--- assets/video.ts
+++ assets/video.ts
@@ -7,9 +7,14 @@ export const channel = {
   videos: [
     {
       id: 1,
-      url: 'https://YHTV-pullzone123.b-cdn.net/susan_b_komen_-_psa..._the_power_of_one%20(720p).mp4',
+      url: `https://YHTV-pullzone123.b-cdn.net/lowe's_ad_-_out_of_the_blue%20(720p).mp4`,
       duration: 40
     },
+    // {
+    //   id: 1,
+    //   url: 'https://YHTV-pullzone123.b-cdn.net/susan_b_komen_-_psa..._the_power_of_one%20(720p).mp4',
+    //   duration: 40
+    // },
     {
       id: 2,
       url: 'https://YHTV-pullzone123.b-cdn.net/test%20folder/332_mill_creek_drive_clayton%2C_nc_27527%20(720p).mp4',
