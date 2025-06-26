855fd3ff2df346d4987004400c366a07f5a877d7
rabius-sunny <rabiussunnyc3x@gmail.com>
2025-06-26 12:12:46 +0600

    update: mobile nav

--- components/ui/mobile-nav.tsx
+++ components/ui/mobile-nav.tsx
@@ -6,26 +6,37 @@ import { cn } from '@/lib/utils';
 import { Menu } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 import { DialogTitle } from './dialog';
 
 export default function MobileNav() {
   const pathname = usePathname();
+  const [open, setOpen] = useState(false);
+
   const isActive = (url: string) => {
     if (url === '/') return pathname === url;
     return pathname.startsWith(url);
   };
 
+  const handleLinkClick = () => {
+    setOpen(false);
+  };
+
   return (
-    <Sheet>
+    <Sheet
+      open={open}
+      onOpenChange={setOpen}
+    >
       <SheetTrigger asChild>
         <Menu className='size-7 data-[state=open]:hidden' />
       </SheetTrigger>
-      <SheetContent className='bg-slate-800/80 text-white p-6 w-full flex items-center justify-center'>
+      <SheetContent className='bg-nav  text-white p-6 w-full flex items-center justify-center'>
         <DialogTitle></DialogTitle>
         {frontendNavData.map((item, idx) => (
           <Link
             key={idx}
             href={item.url}
+            onClick={handleLinkClick}
             className={cn(
               'block text-2xl font-medium text-slate-200 hover:text-sky-300 mb-2',
               isActive(item.url) ? 'text-sky-300' : ''
